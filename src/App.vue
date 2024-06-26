f6f02a9364462ebd90e73ed995493f80495f8e98
matthew-dean-brown <mattrewbrown@gmail.com>
2024-06-26 11:19:35 +0200

    hehe:wheelchair:

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/fetchingData">Fetching Data</router-link> |
   </nav>
   <router-view/>
 </template>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,57 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  // variables
   state: {
+    // data -> propertyName,  null-> value
+    education:null,
+    aboutMe:null,
+    workExp:null,
+    projects:null,
+    testimonials:null,
+    skills:null
+  },
+  mutations: {  
+    // use this to change/update the state
+    // commiting a mutation
+    setAboutMe(state,payload){
+      state.aboutMe = payload
+    },
+    setEducation(state,payload){
+      state.education = payload
+    },
+    setSkills(state,payload){
+      state.skills = payload
+    },
+    setTestimonials(state,payload){
+      state.testimonials = payload
+    },
+    setWorkExp(state,payload){
+      state.workExp = payload
+    },
+    setProjects(state,payload){
+      state.projects = payload
+    }
+  },
+  actions: {
+    // run all async code 
+    //dispatching
+    async getAboutMe({commit}){
+      let fetchedInfo = await fetch('https://matthew-dean-brown.github.io/first_api/data/data.json')
+    let data = await fetchedInfo.json()
+    // dependant on json file
+    let {aboutMe,projects,education,skills,workExp,testimonials} = data
+    console.log(data);
+      commit('setAboutMe',aboutMe)
+      commit('setEducation',education)
+      commit('setSkills',skills)
+      commit('setWorkExp',workExp)
+      commit('setProjects',projects)
+
+    }
+  },
+  modules: {
   },
   getters: {
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
 })
